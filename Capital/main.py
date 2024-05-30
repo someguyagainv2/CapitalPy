11aa7996ee8b939e784797bd729432cf1681c8c5
Malakia Daley <145267660+MalakiaDaley@users.noreply.github.com>
2024-05-30 14:54:44 +0100

    Update main.py

--- Capital/main.py
+++ Capital/main.py
@@ -1,12 +1,61 @@
 import requests
 
 class capital():
+    # SESSION HANDLING
     def __init__(self, token: str=None, email: str=None, password: str=None):
+        # HANDLES SESSION AS BASE
+
+        # CHECKS
+
         if token=="" or token==None: raise ValueError("Token is empty")
         if email=="" or email==None: raise ValueError("Email is empty")
         if password=="" or password==None: raise ValueError("Password is empty")
 
+        # CONSTANTS
 
+        self.APIVersion = "1"
+        self.backendURL = "https://api-capital.backend-capital.com/api/v{0}".format(self.APIVersion)
+        
+        # Request Paramaters
 
+        payload = { "identifier": email, "password": password }
+        self.headers = { "X-CAP-API-KEY": token, "Content-Type": "application/json" }
 
-service = capital("")
+        # Request
+        sessionRequest = requests.post(f"{self.backendURL}/session", headers=self.headers, json=payload)
+
+        # Any errors raise them
+        if sessionRequest.status_code >= 400: raise ValueError(sessionRequest.json()["errorCode"]) # If capital.com returns error status above 400
+        self.accountInfo = sessionRequest.json()
+
+        self.sessionInfo = { "X-SECURITY-TOKEN": sessionRequest.headers.get("X-SECURITY-TOKEN"), "CST": sessionRequest.headers.get("CST") }
+
+    def session(self): # This will get the session information
+        sessionDetailsRequest = requests.get(self.backendURL+"/session", headers=self.sessionInfo)
+        if sessionDetailsRequest.status_code >= 400: raise ValueError(sessionDetailsRequest.json()["errorCode"]) # If capital.com returns error status above 400
+
+        return sessionDetailsRequest.json()
+    
+    def switchaccount(self, accountId: str=None): # Will switch the current account on the session
+        if accountId=="" or accountId==None: raise ValueError("accountId is empty")
+        switchRequest = requests.put(self.backendURL+"/session", headers=self.sessionInfo)
+
+        if switchRequest.status_code >= 400: raise ValueError(switchRequest.json()["errorCode"]) # If capital.com returns error status above 400
+        return switchRequest.json()
+    
+    def destroysession(self): # Destroy the current session you're on
+        sessionRequest = requests.delete(self.backendURL+"/session", headers=self.sessionInfo)
+
+        if sessionRequest.status_code >= 400: raise ValueError(sessionRequest.json()["errorCode"]) # If capital.com returns error status above 400
+        return sessionRequest.json()
+
+    # SESSION END ENDPOINTS
+
+    # ACCOUNT INFORMATION
+
+    def currency(self): # Return currency information
+        return {"type": self.accountInfo["currencyIsoCode"], "symbol": self.accountInfo["currencySymbol"]}
+    
+    def balance(self, currencySymbol: bool=False): # Return the account balance and the currency symbol at the start depending on True or False
+        if currencySymbol: return f'{self.currency["symbol"]}{self.accountInfo["accountInfo"]["balance"]}'
+        else: return f'{self.accountInfo["accountInfo"]["balance"]}'
