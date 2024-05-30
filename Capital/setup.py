d15d2f9bbb7cc18a6579db7b71445213e085f11a
Malakia Daley <145267660+MalakiaDaley@users.noreply.github.com>
2024-05-30 17:43:23 +0100

    Create setup.py

---
+++ Capital/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+name='capital.py',
+author='Malakia',
+author_email='someguyagain1@gmail.com',
+description='This is a easy to use python module for capital.com API',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
